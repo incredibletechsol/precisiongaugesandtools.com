f4d797239ce23d49b0890b6bfd76dd11a36a68d4
Incredible Tech Solutions <incredibletechsol@gmail.com>
2021-02-09 09:12:26 +0530

    Database connection to new hosting

--- ControlPanel/conn.php
+++ ControlPanel/conn.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "kasturic_kasturi";
+$username = "siddhesh_siddhesh";
 $password = "Kasturi254@#$";
-$dbName = "kasturic_precisiontools";
+$dbName = "siddhesh_precision";
 
 // Create connection
 $conn =  mysqli_connect($servername, $username, $password, $dbName);
